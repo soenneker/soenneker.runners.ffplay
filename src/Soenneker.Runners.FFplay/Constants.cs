dbff97ce8d8e990936ef5a985af75f873f58c995
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:26 -0500

    xml commenting

--- src/Soenneker.Runners.FFplay/Constants.cs
+++ src/Soenneker.Runners.FFplay/Constants.cs
@@ -1,7 +1,16 @@
 namespace Soenneker.Runners.FFplay;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The file name.
+    /// </summary>
     public const string FileName = "ffplay.exe";
+    /// <summary>
+    /// The library.
+    /// </summary>
     public const string Library = "Soenneker.Libraries.FFplay";
 }
